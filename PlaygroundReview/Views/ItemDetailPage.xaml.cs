227d6e1a8e3f71fbeda2ade6ebede854a4411497
Logan Stewart <logan.stewart@outlook.com>
2020-10-02 15:36:55 -0600

    Add project files.

---
+++ PlaygroundReview/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using PlaygroundReview.ViewModels;
+
+namespace PlaygroundReview.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ PlaygroundReview/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using PlaygroundReview.Models;
+using PlaygroundReview.ViewModels;
+
+namespace PlaygroundReview.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
